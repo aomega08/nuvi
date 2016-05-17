ccbca2b38650affb07d14360a9651ad44717d6bc
Francesco Boffa <fra.boffa@gmail.com>
2016-05-17 15:08:23 +0200

    Save xml files to redis without dupes

--- lib/nuvi.rb
+++ lib/nuvi.rb
@@ -1,7 +1,9 @@
 require 'nuvi/unzipper'
 require 'nuvi/zip_downloader'
 require 'nuvi/zip_index'
+
 require 'logger'
+require 'redis'
 
 module Nuvi
   extend self
@@ -18,6 +20,17 @@ module Nuvi
     zip_file = downloader.download(zip)
     xml_files_directory = Unzipper.new.unzip(zip_file)
 
+    xmls = Dir[File.join(xml_files_directory, '*.xml')]
+
+    new_items = 0
+    xmls.each do |xml_file|
+      if process_xml(xml_file)
+        new_items += 1
+      end
+    end
+
+    logger.info("Added #{new_items} new items to the NEWS_XML list")
+
     # Cleanup the zip files
     FileUtils.rm_rf(xml_files_directory)
 
@@ -28,11 +41,44 @@ module Nuvi
     #File.delete(zip_file)
   end
 
+  # How it works:
+  #   The NEWS_XML redis list contains all the XML file contents processed
+  #   without duplicates.
+  #   To avoid duplicates we mantain a redis set of the file hashes. If the
+  #   hash is not in the set, it is added and the file content is added to
+  #   the NEWS_XML list.
+  #
+  #   Returns true if the news item is a new entry
+  def process_xml(xml_file)
+    hash = File.basename(xml_file, '.xml')
+
+    # sadd returns true if the element was not in the set
+    if redis.sadd('NEWS_HASHES', hash)
+      content = File.read(xml_file)
+      redis.rpush('NEWS_XML', content)
+      true
+    end
+  end
+
+  def redis
+    @redis ||= Redis.new(host: redis_host, port: redis_port)
+  end
+
   def logger
     @logger ||= Logger.new(STDOUT)
   end
 
   def downloader
     @downloader ||= ZipDownloader.new
+  end
+
+  attr_writer :redis_host, :redis_port
+
+  def redis_host
+    @redis_host ||= 'localhost'
+  end
+
+  def redis_port
+    @redis_port ||= 6379
   end
 end
